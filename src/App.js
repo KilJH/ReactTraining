74090fb3fe6b36a979ddef6cb1d552e02cfd57f2
Jinhyeok Kil <roadjh0201@gmail.com>
2020-12-01 13:07:47 +0900

    Hello-React Chapter 5 ref

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import EventPractice from './EventPractice';
+//import ValidationSample from './VaildationSample';
+import ScrollBox from './ScrollBox';
 
-const App = () => {
-	return <EventPractice />;
-};
+//const App = () => {
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<ScrollBox
+					ref={(ref) => {
+						this.scrollBox = ref;
+					}}
+				/>
+				<button onClick={() => this.scrollBox.scrollToBottom()}>
+					맨밑으로
+				</button>
+			</div>
+		);
+	}
+}
 
 export default App;
